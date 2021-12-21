676ea62582ad8d4d8241fc4e6c4c8d116134eaac
zakariamosa <zakariamosa1211@gmail.com>
2021-12-21 17:27:27 +0100

    first commit

---
+++ Lab2/Entities/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace Animal.Entities
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string AnimalType { get; set; }
+        public string Name { get; set; }
+
+    }
+}
